d4e94c09bef910f177f23e168638ba56dc4c32ef
suyeon <suyeon9456@naver.com>
2022-09-09 13:16:43 +0900

    [FEAT] 스크롤 위치와 정보 STATE 생성

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,5 +1,6 @@
 import { atom, selector } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
+import { Product } from '../types/product';
 import { LoginResType } from '../types/user';
 
 const sessionStorage = typeof window !== `undefined` ? window.sessionStorage : undefined;
@@ -15,15 +16,23 @@ export const myinfoState = atom<LoginResType | null>({
   effects_UNSTABLE: [persistAtom],
 });
 
-// export const myinfoState = selector({
-//   key: 'myinfoState',
-//   get: ({ get }) => {
-//     const info = get(myinfo);
-//     return info;
-//   },
-// });
-
 export const pageState = atom<number>({
   key: 'productPage',
   default: 1,
+});
+
+export const infiniteScrollState = atom<{
+  products: Product[],
+  scrollY: number, 
+}>({
+  key: 'infiniteScroll',
+  default: {
+    products: [],
+    scrollY: 0, 
+  },
+});
+
+export const prevPageState = atom<string>({
+  key: 'productprevPage',
+  default: '',
 });
